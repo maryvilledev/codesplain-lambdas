fc16fa443ddfa0c81421486ec7459629173bbaaa
JMH <jheld135@gmail.com>
2017-04-07 11:00:53 -0500

    Refactor SaveSnippetToS3 lambda to append numbers to titles that pre-exist

--- lambdas/SaveSnippetToS3/index.js
+++ lambdas/SaveSnippetToS3/index.js
@@ -34,6 +34,61 @@ const saveToS3 = (Bucket, Key, Body) => {
   });
 }
 
+const checkIfKeyIsUsed = (Bucket, Key) => {
+  const params = {
+    Bucket,
+    Key,
+  };
+  return new Promise((resolve, reject) => {
+    s3.headObject(params, (err, data) => {
+      if (err) {
+        if (err.code === 'NotFound') {
+          resolve(false);
+        } else {
+          reject(err);
+        }
+      } else {
+        resolve(true);
+      }
+    });
+  });
+}
+
+const retryGenerateSnippetKey = (bucket, userID, snippetKey) => {
+  return new Promise((resolve, reject) => {
+    checkIfKeyIsUsed(bucket, `${userID}/${snippetKey}`)
+      .then((isUsed) => {
+        if (isUsed) {
+          // If the string ends with _(NUMBER), add 1 to the number. Otherwise,
+          // append _1
+          const appendedNumberRegex = /(.*_)(\d+)$/gm;
+          const result = appendedNumberRegex.exec(snippetKey);
+          let newKey;
+          if (result) {
+            newKey = `${result[1]}${Number(result[2]) + 1}`;
+          } else {
+            newKey = `${snippetKey}_1`;
+          }
+          // Resolve the promise with the results of the retried
+          // generateSnippetKey call
+          resolve(retryGenerateSnippetKey(bucket, userID, newKey));
+        } else {
+          // Key is unused; resolve the promise with it
+          resolve(snippetKey);
+        }
+      }, (err) => {
+        // Reject the promise with the err
+        reject(err);
+      });
+  });
+}
+
+const generateSnippetKey = (bucket, userID, snippetTitle) => {
+  // Encode the snippetKey
+  const snippetKey = encodeURIComponent(snippetTitle.replace(/\s+/g, '_').toLowerCase());
+  return retryGenerateSnippetKey(bucket, userID, snippetKey);
+}
+
 exports.handler = (event, context, callback) => {
   // Extract some stuff that we need from the request object.
   const accessToken = event.headers.Authorization;
@@ -65,28 +120,31 @@ exports.handler = (event, context, callback) => {
     }
 
     /* ----- Otherwise, save to S3 ----- */
-    const body       = JSON.parse(event.body);
-    const snippetKey = encodeURIComponent(body.snippetTitle.replace(/\s+/g, '_').toLowerCase());
-    const key        = `${userID}/${snippetKey}`;
     const apiID      = event.requestContext.apiId;
+    const body       = JSON.parse(event.body);
     const bucket     = process.env.BucketName;
-
-    saveToS3(bucket, key, event.body)
-      .then(key => {
-        callback(null, {
-          statusCode: '200',
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-          },
-          body: `{ "key": "${snippetKey}" }`,
-        })
+    generateSnippetKey(bucket, userID, body.snippetTitle)
+      .then((snippetKey) => {
+        const key = `${userID}/${snippetKey}`;
+        saveToS3(bucket, key, event.body)
+          .then(() => {
+            callback(null, {
+              statusCode: '200',
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+              },
+              body: `{ "key": "${snippetKey}" }`,
+            })
+          })
+          .catch(err => {
+            const message = `Error putting object ${key} into bucket ${bucket}.` +
+                            `Make sure they exist and your bucket is in the same ` +
+                            `region as this function.`;
+            console.log(message);
+            sendResponse(callback, '500', 'Error saving snippet.');
+          });
       })
-      .catch(err => {
-        const message = `Error putting object ${key} into bucket ${bucket}.` +
-                        `Make sure they exist and your bucket is in the same ` +
-                        `region as this function.`;
-        console.log(message);
-        sendResponse(callback, '500', 'Error saving snippet.');
-      });
+
+
   });
 };
