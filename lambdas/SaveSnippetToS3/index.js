d01c47f836823afb9275b8bd9b0c8ba97f9184b2
Jordan Davis <Jordan.davis@maryville.com>
2017-04-10 14:26:42 -0500

    Replaced node lambda with python

--- lambdas/SaveSnippetToS3/index.js
+++ lambdas/SaveSnippetToS3/index.js
@@ -1,162 +1,75 @@
-'use strict';
+import json
+import urllib
+import re
+import os
+from datetime import datetime
+from boto3.s3.transfer import ClientError
+import string
+import boto3 # AWS SDK for Python
 
-console.log('Loading function');
+s3 = boto3.client('s3', 'us-west-2')
+client = boto3.client('lambda')
 
-const aws = require('aws-sdk');
-const s3 = new aws.S3({ apiVersion: '2006-03-01' });
-const lambda = new aws.Lambda({
-  region: 'us-west-2',
-});
+#Verifies existance of index file; if absent, creates index file. Writes file to S3.
+def updateIndexFile(snippetKey, bucket, snippetData, user):
+    try:
+        indexData = s3.get_object(Bucket=bucket, Key=user + '/index.json')['Body'].read()
+    except ClientError as error:
+        indexData = '{}'
 
-// Helper for invoking the callback in handler func
-const sendResponse = (callback, statusCode, response) => {
-  callback(null, {
-    statusCode,
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-    },
-    body: `{ "response": "${response}" }`,
-  })
-}
+    index = json.loads(indexData)
+    index[snippetKey] = snippetData
+    s3.put_object(Body=json.dumps(index), Bucket=bucket, Key=user +'/index.json')
 
-// Saves the given Body to the given Bucket under the given Key. Returns a Promise.
-const saveToS3 = (Bucket, Key, Body) => {
-  const params = {
-      Bucket,
-      Key,
-      Body,
-  };
-  return new Promise((resolve, reject) => {
-    s3.putObject(params, (err, data) => {
-      if (err) reject(err);
-      resolve({ Key });
-    });
-  });
-}
+#Saves the given body to the appropiate bucket under the assigned key.
+def saveToS3(bucket, key, body):
+    try:
+        s3.put_object(Body=body, Bucket=bucket, Key=key)
+    except ClientError as error:
+        err_code = str(error.response['Error']['Code'])
+        print 'Error putting object %s into bucket %s. Make sure they exist and ' \
+              'your bucket is in the same region as this function.' % (key, bucket)
+        raise error
 
-const checkIfKeyIsUsed = (Bucket, Key) => {
-  const params = {
-    Bucket,
-    Key,
-  };
-  return new Promise((resolve, reject) => {
-    s3.headObject(params, (err, data) => {
-      if (err) {
-        if (err.code === 'NotFound') {
-          resolve(false);
-        } else {
-          reject(err);
-        }
-      } else {
-        resolve(true);
-      }
-    });
-  });
-}
+def lambda_handler(event, context):
+    #Extract needed data from the event
+    accessToken = event['headers']['Authorization']
+    userID = event['pathParameters']['user_id']
 
-const retryGenerateSnippetKey = (bucket, userID, snippetKey) => {
-  return new Promise((resolve, reject) => {
-    checkIfKeyIsUsed(bucket, `${userID}/${snippetKey}`)
-      .then((isUsed) => {
-        if (isUsed) {
-          // If the string ends with _(NUMBER), add 1 to the number. Otherwise,
-          // append _1
-          const appendedNumberRegex = /(.*_)(\d+)$/gm;
-          const result = appendedNumberRegex.exec(snippetKey);
-          let newKey;
-          if (result) {
-            newKey = `${result[1]}${Number(result[2]) + 1}`;
-          } else {
-            newKey = `${snippetKey}_1`;
-          }
-          // Resolve the promise with the results of the retried
-          // generateSnippetKey call
-          resolve(retryGenerateSnippetKey(bucket, userID, newKey));
-        } else {
-          // Key is unused; resolve the promise with it
-          resolve(snippetKey);
-        }
-      }, (err) => {
-        // Reject the promise with the err
-        reject(err);
-      });
-  });
-}
+    authorizeTokenName = os.environ['authorizeTokenName']
 
-const generateSnippetKey = (bucket, userID, snippetTitle) => {
-  // Encode the snippetKey
-  const snippetKey = encodeURIComponent(snippetTitle.replace(/\s+/g, '_').toLowerCase());
-  return retryGenerateSnippetKey(bucket, userID, snippetKey);
-}
+    #Invoke the auth. lambda to verify the accessToken mathches the userID for the requested resource
+    lambda_auth = client.invoke(
+        FunctionName=os.environ['authorizeTokenName'],
+        Payload=json.dumps({'accessToken': accessToken, 'userID': userID}))
 
-const generateBody = (body, snippetKey) => {
-  // Update the title of the snippet if the key it's saved under differs from it
-  if (body.snippetTitle !== snippetKey) {
-    return Object.assign({}, body, {
-      snippetTitle: snippetKey
-    });
-  }
-  return body;
-}
+    lambda_payload_resp = json.loads(lambda_auth['Payload'].read())
 
-exports.handler = (event, context, callback) => {
-  // Extract some stuff that we need from the request object.
-  const accessToken = event.headers.Authorization;
-  const userID      = event.pathParameters.user_id;
+    if(lambda_payload_resp['statusCode'] == '400'):
+        return {'statusCode': '400', 'body': json.dumps({'response': lambda_payload_resp['body']})}
 
-  // Invoke the authorization lambda to ensure the accessToken
-  // included in the request matches the userID for the requested
-  // resource.
-  const authorizeTokenName = process.env.authorizeTokenName;
-  lambda.invoke({
-    FunctionName: authorizeTokenName,
-    Payload: JSON.stringify({
-      accessToken,
-      userID,
-    }),
-  }, (err, data) => {
-    // Handle the error if the AuthorizeToken lambda failed
-    // to finish properly.
-    if (err) {
-      console.log(err);
-      sendResponse(callback, '500', 'Failed to save snippet.');
+    # -------- Otherwise, save to S3 -------- #
+
+    body = json.loads(event['body'])
+    snippetTitle = body['snippetTitle']
+    snippetKey = urllib.quote(string.lower(re.sub(r'\s+', '_', snippetTitle)))
+    key = userID + '/' + snippetKey
+    apiID = event['requestContext']['apiId']
+    bucket = os.environ['BucketName']
+
+    if(bucket == None):
+        print 'Unrecognized apiId: %s' % (apiId)
+        raise error
+    else:
+        saveToS3(bucket, key, event['body'])
+
+    updateIndexFile(
+        snippetKey,
+        bucket,
+        {'snippetTitle': snippetTitle, 'language': 'python3', 'lastEdited': datetime.now().isoformat()},
+        userID)
+    return {
+        'statusCode': '200',
+        'headers': {'Access-Control-Allow-Origin': '*'},
+        'body': json.dumps({'key': snippetKey})
     }
-
-    const payload = JSON.parse(data.Payload);
-    // If authorization failed, respond with a 400
-    if (payload.statusCode === '400') {
-      console.log(payload.body);
-      sendResponse(callback, '400', payload.body);
-    }
-
-    /* ----- Otherwise, save to S3 ----- */
-    const apiID      = event.requestContext.apiId;
-    const body       = JSON.parse(event.body);
-    const bucket     = process.env.BucketName;
-    generateSnippetKey(bucket, userID, body.snippetTitle)
-      .then((snippetKey) => {
-        const key = `${userID}/${snippetKey}`;
-        // Update the save body
-        const saveBody = generateBody(body, snippetKey);
-        saveToS3(bucket, key, JSON.stringify(saveBody))
-          .then(() => {
-            callback(null, {
-              statusCode: '200',
-              headers: {
-                'Access-Control-Allow-Origin': '*',
-              },
-              body: `{ "key": "${snippetKey}" }`,
-            })
-          }, (err) => { console.log(err); })
-          .catch(err => {
-            const message = `Error putting object ${key} into bucket ${bucket}.` +
-                            `Make sure they exist and your bucket is in the same ` +
-                            `region as this function.`;
-            console.log(message);
-            sendResponse(callback, '500', 'Error saving snippet.');
-          });
-      })
-
-
-  });
-};
