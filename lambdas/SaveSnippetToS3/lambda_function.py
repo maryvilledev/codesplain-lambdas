fbffee8191e5c83843073449fc2a6dd40942835f
Jordan Davis <Jordan.davis@maryville.com>
2017-04-11 13:17:34 -0500

    Fixed python save labmda

--- lambdas/SaveSnippetToS3/lambda_function.py
+++ lambdas/SaveSnippetToS3/lambda_function.py
@@ -11,7 +11,7 @@ s3 = boto3.client('s3', 'us-west-2')
 client = boto3.client('lambda')
 
 #Verifies existance of index file; if absent, creates index file. Writes file to S3.
-def updateIndexFile(snippetKey, bucket, snippetData, user):
+def update_index_file(snippetKey, bucket, snippetData, user):
     try:
         indexData = s3.get_object(Bucket=bucket, Key=user + '/index.json')['Body'].read()
     except ClientError as error:
@@ -22,7 +22,7 @@ def updateIndexFile(snippetKey, bucket, snippetData, user):
     s3.put_object(Body=json.dumps(index), Bucket=bucket, Key=user +'/index.json')
 
 #Saves the given body to the appropiate bucket under the assigned key.
-def saveToS3(bucket, key, body):
+def save_to_s3(bucket, key, body):
     try:
         s3.put_object(Body=body, Bucket=bucket, Key=key)
     except ClientError as error:
@@ -36,8 +36,6 @@ def lambda_handler(event, context):
     accessToken = event['headers']['Authorization']
     userID = event['pathParameters']['user_id']
 
-    authorizeTokenName = os.environ['authorizeTokenName']
-
     #Invoke the auth. lambda to verify the accessToken mathches the userID for the requested resource
     lambda_auth = client.invoke(
         FunctionName=os.environ['authorizeTokenName'],
@@ -49,21 +47,27 @@ def lambda_handler(event, context):
         return {'statusCode': '400', 'body': json.dumps({'response': lambda_payload_resp['body']})}
 
     # -------- Otherwise, save to S3 -------- #
-
+    if(event['body'] == None):
+        return {
+            'statusCode': '400',
+            'headers':    { 'Access-Control-Allow-Origin': '*' },
+            'body':       json.dumps({
+               'response': 'POST requests must not have empty bodies.'
+            })
+        }
     body = json.loads(event['body'])
     snippetTitle = body['snippetTitle']
     snippetKey = urllib.quote(string.lower(re.sub(r'\s+', '_', snippetTitle)))
     key = userID + '/' + snippetKey
-    apiID = event['requestContext']['apiId']
+
     bucket = os.environ['BucketName']
-
     if(bucket == None):
-        print 'Unrecognized apiId: %s' % (apiId)
+        print 'Must specify "BucketName" env var!'
         raise error
-    else:
-        saveToS3(bucket, key, event['body'])
 
-    updateIndexFile(
+    save_to_s3(bucket, key, event['body'])
+
+    update_index_file(
         snippetKey,
         bucket,
         {'snippetTitle': snippetTitle, 'language': 'python3', 'lastEdited': datetime.now().isoformat()},
