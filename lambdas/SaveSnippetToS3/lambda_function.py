c8dab5badd7a8426f7065ad5269909a254f172e2
Hopding <andrew.dillon.j@gmail.com>
2017-04-12 13:15:10 -0500

    Updated Update and Save lambdas to use similar style and functions

--- lambdas/SaveSnippetToS3/lambda_function.py
+++ lambdas/SaveSnippetToS3/lambda_function.py
@@ -7,46 +7,57 @@ from boto3.s3.transfer import ClientError
 import string
 import boto3 # AWS SDK for Python
 
-s3 = boto3.client('s3', 'us-west-2')
+s3     = boto3.client('s3', 'us-west-2')
 client = boto3.client('lambda')
 
-#Verifies existance of index file; if absent, creates index file. Writes file to S3.
-def update_index_file(snippet_key, bucket, snippet_data, user):
+# Updates index file and writes to S3. Creates new one if needed.
+def update_index_file(bucket, user_id, snippet_key, entry):
+    key = user_id + '/index.json'
     try:
-        index_data = s3.get_object(Bucket=bucket, Key=user + '/index.json')['Body'].read()
+        index_str = s3.get_object(Bucket=bucket, Key=key)['Body'].read()
     except ClientError as error:
-        index_data = '{}'
+        index_str = '{}'
 
-    index = json.loads(index_data)
-    index[snippet_key] = snippet_data
-    s3.put_object(Body=json.dumps(index), Bucket=bucket, Key=user +'/index.json')
+    index = json.loads(index_str)
+    index[snippet_key] = entry
+    s3.put_object(Body=json.dumps(index), Bucket=bucket, Key=key)
 
-#Saves the given body to the appropiate bucket under the assigned key.
-def save_to_s3(bucket, key, body):
+# Saves the given body to the given bucket under the given key
+def save_to_s3(bucket, user_id, snippet_key, body):
+    key = user_id + '/' + snippety_key
     try:
         s3.put_object(Body=body, Bucket=bucket, Key=key)
     except ClientError as error:
-        err_code = str(error.response['Error']['Code'])
-        print 'Error putting object %s into bucket %s. Make sure they exist and ' \
-              'your bucket is in the same region as this function.' % (key, bucket)
+        print 'Error putting object %s into bucket %s. Make sure your bucket ' \
+        'exists and is in the same region as this function.' % (key, bucket)
         raise error
 
+# Lambda handler function
 def lambda_handler(event, context):
-    #Extract needed data from the event
+    # Extract needed data from the event
     access_token = event['headers']['Authorization']
-    user_id = event['pathParameters']['user_id']
+    user_id      = event['pathParameters']['user_id']
 
-    #Invoke the auth. lambda to verify the accessToken mathches the user_id for the requested resource
+    # Invoke the authorization lambda to verify the
+    # accessToken matches the user_id for the requested resource
     lambda_auth = client.invoke(
         FunctionName=os.environ['authorizeTokenName'],
-        Payload=json.dumps({'accessToken': access_token, 'userID': user_id}))
+        Payload=json.dumps({
+            'accessToken': access_token,
+            'userID': user_id
+        }))
 
-    lambda_payload_resp = json.loads(lambda_auth['Payload'].read())
+    # Respond with 400 is user is not authorized
+    resp_payload = json.loads(lambda_auth['Payload'].read())
+    if(resp_payload['statusCode'] == '400'):
+        return {
+            'statusCode': '400',
+            'body': json.dumps({
+                'response': lambda_payload_resp['body']
+            })
+        }
 
-    if(lambda_payload_resp['statusCode'] == '400'):
-        return {'statusCode': '400', 'body': json.dumps({'response': lambda_payload_resp['body']})}
-
-    # -------- Otherwise, save to S3 -------- #
+    # -------- Otherwise, user is authorized, so save to S3 -------- #
     if(event['body'] == None):
         return {
             'statusCode': '400',
@@ -55,25 +66,22 @@ def lambda_handler(event, context):
                'response': 'POST requests must not have empty bodies.'
             })
         }
-    body = json.loads(event['body'])
-    snippet_title = body['snippetTitle']
-    snippet_key = urllib.quote(string.lower(re.sub(r'\s+', '_', snippet_title)))
-    key = user_id + '/' + snippet_key
-
+    body         = json.loads(event['body'])
+    snippet_id   = urllib.quote(string.lower(re.sub(r'\s+', '_', snippet_title)))
     bucket = os.environ['BucketName']
     if(bucket == None):
         print 'Must specify "BucketName" env var!'
         raise error
 
-    save_to_s3(bucket, key, event['body'])
-
-    update_index_file(
-        snippet_key,
-        bucket,
-        {'snippetTitle': snippet_title, 'language': body['snippetLanguage'], 'lastEdited': datetime.now().isoformat()},
-        user_id)
+    save_to_s3(bucket, user_id, snippet_id, event['body'])
+    new_entry = {
+        'snippetTitle': body['snippetTitle'],
+        'language':     body['snippetLanguage'],
+        'lastEdited':   datetime.now().isoformat()
+    }
+    update_index_file(bucket, user_id, snippet_id, new_entry)
     return {
         'statusCode': '200',
-        'headers': {'Access-Control-Allow-Origin': '*'},
-        'body': json.dumps({'key': snippet_key})
+        'headers':    { 'Access-Control-Allow-Origin': '*' },
+        'body':       json.dumps({ 'key': snippet_key })
     }
