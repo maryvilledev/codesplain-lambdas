3497161a416a8b6f5236c4a3d11bde2527027001
Brian Hoehne <brian.hoehne@maryville.com>
2017-04-17 14:53:55 -0500

    add validation

--- scripts/featureBranchName.py
+++ scripts/featureBranchName.py
@@ -1,9 +1,19 @@
-import string, sys
+import string, sys, json
 
 def main(arg1):
-    print (arg1)
-    maryvilledev_start = string.find(arg1,"maryvilledev") + 13
-    feature_branch_end = string.find(arg1, " ", maryvilledev_start)
+    print arg1
+    message = eval(arg1)['message']
+    print message
+    maryvilledev_start = string.find(message,'maryvilledev/')
+    if maryvilledev_start == -1:
+        print("Missing marvyilledev/")
+        return -1
+    maryvilledev_start+=13
+    feature_branch_end = string.find(message, ' ', maryvilledev_start)
+    if feature_branch_end == -1:
+        print("Missing feature branch end")
+        return -1
+    print(maryvilledev_start,feature_branch_end)
     print arg1[maryvilledev_start: feature_branch_end]
 
 if __name__=='__main__':
