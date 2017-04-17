41ee5778bd81cc083deb5491839f6f52cbe17e5c
Brian Hoehne <brian.hoehne@maryville.com>
2017-04-17 14:21:42 -0500

    pass message to python file

--- scripts/featureBranchName.py
+++ scripts/featureBranchName.py
@@ -1,6 +1,7 @@
 import string, sys
 
 def main(arg1):
+    print (arg1)
     maryvilledev_start = string.find(arg1,"maryvilledev") + 13
     feature_branch_end = string.find(arg1, " ", maryvilledev_start)
     print arg1[maryvilledev_start: feature_branch_end]
