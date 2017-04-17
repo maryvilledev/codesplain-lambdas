6e619331ca44f523a76df66c8f5c3f898bbfd159
Brian Hoehne <brian.hoehne@maryville.com>
2017-04-17 15:47:10 -0500

    fix python returns

--- scripts/featureBranchName.py
+++ scripts/featureBranchName.py
@@ -4,12 +4,14 @@ def main(arg1):
     message = eval(arg1)['message']
     maryvilledev_start = string.find(message,'maryvilledev/')
     if maryvilledev_start == -1:
-        return -1
+        print -1
+        return
     maryvilledev_start+=13
     feature_branch_end = string.find(message, ' ', maryvilledev_start)
     if feature_branch_end == -1:
-        return -1
-    print arg1[maryvilledev_start: feature_branch_end]
+        print -1
+        return
+    print message[maryvilledev_start: feature_branch_end]
 
 if __name__=='__main__':
     main(sys.argv[1])
