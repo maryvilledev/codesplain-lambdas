56a7fce9acb4da2070e4617ce4e3e55cb1cd389c
Jordan Davis <Jordan.davis@maryville.com>
2017-04-17 12:31:53 -0500

    Work In Progress

--- lambdas/Authorize/lambda_funtion.py
+++ lambdas/Authorize/lambda_funtion.py
@@ -1,6 +1,6 @@
 import os
 import json
-import requests
+import requests.auth import HTTPBasicAuth
 from boto3.s3.transfer import ClientError
 
 s3 = boto3.client('s3', 'us-west-2')
@@ -8,6 +8,9 @@ client = boto3.client('lambda')
 
 client_id = os.environ['CLIENT_ID']
 client_secret = os.environ['CLIENT_SECRET']
+
+def auth_request(client_id, client_secret):
+    requests.get('https://api.github.com/applications/ %s/tokens/ %s' % (client_id, token), auth=('client_id', 'client_secret'))
 
 def generate_policy(prinicipal_id, effect, resource):
     return {
@@ -25,7 +28,8 @@ def generate_policy(prinicipal_id, effect, resource):
 def lambda_handler(event, context):
     token = event['authorizationToken']
     try:
-        requests.get('https://api.github.com/applications/ %s/tokens/ %s' % (client_id, token), auth=('client_id', 'token'))
-        generate_policy('user', 'Allow', event[methodArn])
+        auth_request(token)
     except ClientError as error:
         print 'Unauthorized'
+
+    generate_policy('user', 'Allow', event[methodArn])
