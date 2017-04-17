84848455a081c9ffa8f7568122b1a492952ff386
Brian Hoehne <brian.hoehne@maryville.com>
2017-04-17 13:19:58 -0500

    try deteling stack right away

---
+++ scripts/featureBranchName.py
@@ -0,0 +1,9 @@
+import string, sys
+
+def main(arg1):
+    maryvilledev_start = string.find(arg1,"maryvilledev") + 13
+    feature_branch_end = string.find(arg1, " ", maryvilledev_start)
+    print arg1[maryvilledev_start: feature_branch_end]
+
+if __name__=='__main__':
+    main(sys.argv[1])
