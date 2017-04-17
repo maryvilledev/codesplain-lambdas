3ddbd66666b051aef57e7edbaf9a6f10ff0eb323
Brian Hoehne <brian.hoehne@maryville.com>
2017-04-17 15:56:23 -0500

    refactor python

--- scripts/featureBranchName.py
+++ scripts/featureBranchName.py
@@ -1,17 +1,26 @@
 import string, sys, json
 
-def main(arg1):
-    message = eval(arg1)['message']
+def findFeatureFromMessage(commitBody):
+    print commitBody
+    message = eval(commitBody)['message']
+    print message
     maryvilledev_start = string.find(message,'maryvilledev/')
     if maryvilledev_start == -1:
-        print -1
-        return
+        print "Start missing"
+        return -1
     maryvilledev_start+=13
     feature_branch_end = string.find(message, ' ', maryvilledev_start)
     if feature_branch_end == -1:
-        print -1
-        return
-    print message[maryvilledev_start: feature_branch_end]
+        return -1
+
+    feature = message[maryvilledev_start: feature_branch_end]
+    print feature
+    return feature
+
+def main(arg1):
+    message = eval(arg1)['message']
+
+    print findFeatureFromMessage(arg1)
 
 if __name__=='__main__':
     main(sys.argv[1])
