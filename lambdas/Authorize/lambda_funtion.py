c4007fce0b554f42731448c428c1b896f7a13078
Jordan Davis <Jordan.davis@maryville.com>
2017-04-17 14:53:54 -0500

    Added python dependencies to publish script

--- lambdas/Authorize/lambda_funtion.py
+++ lambdas/Authorize/lambda_funtion.py
@@ -12,10 +12,10 @@ client_secret = os.environ['CLIENT_SECRET']
 def auth_request(client_id, client_secret):
     requests.get('https://api.github.com/applications/ %s/tokens/ %s' % (client_id, token), auth=('client_id', 'client_secret'))
 
-def generate_policy(principal_id, effect, resource):
+def generate_policy(principalId, effect, resource):
     return {
-        'principal_id',
-        'policy_document': {
+        'principalId',
+        'policyDocument': {
           'Version': '2012-10-17',
           'Statement': [{
             'Action': 'execute-api:Invoke',
@@ -25,7 +25,6 @@ def generate_policy(principal_id, effect, resource):
         }
     }
 
-
 def lambda_handler(event, context):
     token = event['authorizationToken']
     try:
@@ -33,5 +32,4 @@ def lambda_handler(event, context):
     except ClientError as error:
         print 'Unauthorized'
 
-    generate_policy('user', 'Allow', event[methodArn])
-    print "TEST"
+    generate_policy(user, Allow, event[methodArn])
