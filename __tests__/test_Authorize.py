a4727de106fa0ce87e2ea04552f5176bc5ecb519
Jordan Davis <Jordan.davis@maryville.com>
2017-04-18 15:25:15 -0500

    Fixed authorize lambda and added unit test for it

---
+++ __tests__/test_Authorize.py
@@ -0,0 +1,27 @@
+import sys
+import os
+import unittest
+from mock import patch, Mock
+
+with patch('boto3.client'):
+    os.environ['CLIENT_ID'] = 'MockClientId'
+    os.environ['CLIENT_SECRET']= 'FooBar'
+    import lambdas.Authorize.lambda_function as auth
+
+class TestAuthorize(unittest.TestCase):
+    @patch('boto3.client')
+    def setUp (self, mock_boto3):
+        ## We're going to mock out some global functions, so re-import before each
+        reload(auth)
+        ## Mock out the s3 object
+        auth.s3 = Mock()
+
+    @patch('requests.get')
+    def test_auth_request(self, mock_requests):
+        mock_client_id = 'Thing1'
+        mock_client_secret = 'Thing2'
+        mock_token = '123'
+
+        auth.auth_request(mock_client_id, mock_client_secret, mock_token)
+
+        mock_requests.assert_called_once_with('https://api.github.com/applications/%s/tokens/%s' % (mock_client_id, mock_token), auth=(mock_client_id, mock_client_secret))
--- lambdas/Authorize/lambda_function.py
+++ lambdas/Authorize/lambda_function.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+import boto3
 from boto3.s3.transfer import ClientError
 
 s3 = boto3.client('s3', 'us-west-2')
@@ -9,12 +10,12 @@ client = boto3.client('lambda')
 client_id = os.environ['CLIENT_ID']
 client_secret = os.environ['CLIENT_SECRET']
 
-def auth_request(client_id, client_secret):
-    requests.get('https://api.github.com/applications/ %s/tokens/ %s' % (client_id, token), auth=('client_id', 'client_secret'))
+def auth_request(client_id, client_secret, token):
+    requests.get('https://api.github.com/applications/%s/tokens/%s' % (client_id, token), auth=(client_id, client_secret))
 
 def generate_policy(principalId, effect, resource):
     return {
-        'principalId',
+        'principalId':principalId,
         'policyDocument': {
           'Version': '2012-10-17',
           'Statement': [{
