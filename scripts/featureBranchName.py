f43b0d6ecae1ac1cf7fd3d80a5004a8dfc652c61
Brian Hoehne <brian.hoehne@maryville.com>
2017-04-18 09:09:30 -0500

    try returns

--- scripts/featureBranchName.py
+++ scripts/featureBranchName.py
@@ -3,10 +3,8 @@ import string, sys, json
 def findFeatureFromMessage(commitBody):
     commitBody = json.loads(commitBody)
     message = commitBody['message']
-    print message
     maryvilledev_start = string.find(message,'maryvilledev/')
     if maryvilledev_start == -1:
-        print "Start missing"
         return -1
     maryvilledev_start+=13
     feature_branch_end = string.find(message, ' ', maryvilledev_start)
@@ -14,11 +12,10 @@ def findFeatureFromMessage(commitBody):
         return -1
 
     feature = message[maryvilledev_start: feature_branch_end]
-    print feature
     return feature
 
 def main(arg1):
-    return findFeatureFromMessage(arg1)
+    print findFeatureFromMessage(arg1)
 
 if __name__=='__main__':
     main(sys.argv[1])
