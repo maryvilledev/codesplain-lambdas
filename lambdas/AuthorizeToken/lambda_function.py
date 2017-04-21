5cd3d427ecd77bdb3cb202df80a75e13f5124501
Jordan Davis <Jordan.davis@maryville.com>
2017-04-21 12:36:09 -0500

    Started converting AuthorizeToken

---
+++ lambdas/AuthorizeToken/lambda_function.py
@@ -0,0 +1,20 @@
+import os
+import json
+import requests
+import boto3
+from boto3.s3.transfer import ClientError
+
+s3 = boto3.client('s3', 'us-west-2')
+client = boto3.client('lambda')
+
+def authorize_token(acces_token, username):
+    headers = {'Accept': 'application/json', 'Authorization': token['acces_token']}
+    url = 'https://api.github.com/user'
+    try:
+        requests.get(url, headers=headers)
+        return {'statusCode': '200', 'body': 'Authorized to access this resource'}
+    except ClientError as error:
+        return {'statusCode': '400', 'body': 'Not authorized to access this resource'}
+    if 'statusCode' == '500':
+        except ClientError as error:
+            return {'statusCode': '500', 'body': json.dumps({'response': 'response'})}           #TODO:// Do better than this
