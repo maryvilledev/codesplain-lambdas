aa6981549820a9b2796c107285f91e1af215f5a5
Hopding <andrew.dillon.j@gmail.com>
2017-04-24 14:51:52 -0500

    Add bash script to run tests in order

--- api_tests/config.py
+++ api_tests/config.py
@@ -2,6 +2,6 @@ import json
 from os.path import dirname, abspath
 
 def parse():
-    config_path = dirname(__file__) + '/config.json'
+    config_path = dirname(abspath(__file__)) + '/config.json'
     config_dict = json.load(open(config_path, 'r'))
     return config_dict
