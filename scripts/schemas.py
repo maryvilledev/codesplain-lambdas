19c4c6d94e7e129585b8d46f578abdff48ddc030
JMH <jheld135@gmail.com>
2017-05-03 11:47:46 -0500

    Add AST and Snippet schemas

--- scripts/schemas.py
+++ scripts/schemas.py
@@ -1,15 +1,32 @@
 from marshmallow import Schema, fields
 
 class Filter(Schema):
-    color: fields.String()
-    count: fields.Integer()
-    prettyTokenName: fields.String()
-    selected: fields.Boolean()
+    color = fields.String(required=True)
+    count = fields.Integer(required=True)
+    prettyTokenName = fields.String(required=True)
+    selected = fields.Boolean(required=True)
 
 class Annotation(Schema):
-    annoation: fields.String()
-    lineNumber: fields.Integer()
-    lineText: fields.String()
+    annotation = fields.String(required=True)
+    lineNumber = fields.Integer(required=True)
+    lineText = fields.String(required=True)
 
 class Annotations(Schema):
-    annotations: fields.Nested(Annotation, many=True)
+    annotations = fields.Nested(Annotation, many=True, required=True)
+
+class AST(Schema):
+    begin = fields.Number(required=True)
+    end = fields.Number(required=True)
+    tags = fields.List(fields.String(), required=True)
+    nodeType = fields.String(attribute="type", required=True)
+    children = fields.List(fields.Nested("self", many=True), required=True)
+
+class Snippet(Schema):
+    annotations = fields.Nested(Annotations, required=True)
+    AST = fields.Nested(AST, required=True)
+    filters = fields.Nested(Filter, many=True, required=True)
+    readOnly = fields.Boolean(required=True)
+    snippet = fields.String(required=True)
+    snippetKey = fields.String(required=True)
+    snippetLanguage = fields.String(required=True)
+    snippetTitle = fields.String(required=True)
