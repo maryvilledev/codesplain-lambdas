b833acb6f811231a4dde5f44980ff5b18579f46c
Hopding <andrew.dillon.j@gmail.com>
2017-05-04 17:08:13 -0500

    Add logging

--- lambdas/GetSnippetFromS3/lambda_function.py
+++ lambdas/GetSnippetFromS3/lambda_function.py
@@ -14,10 +14,12 @@ def lambda_handler(event, context):
         print 'Must specify "BucketName" env var!'
         raise error
     try:
+        print 'Bucket=%s' % bucket
+        print 'Key=%s' % key
         object = s3.get_object(Bucket=bucket, Key=key)['Body'].read()
-        print object
     except ClientError as error:
         print 'Error getting object %s from bucket %s' % (key, bucket)
+        print error
         return {
                 'statusCode': '400',
                 'headers': {'Access-Control-Allow-Origin': '*'}
