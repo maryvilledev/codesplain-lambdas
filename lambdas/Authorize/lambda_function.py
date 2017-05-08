c6f673ea53e884ea80514b616b58488e4e72c785
Jordan Davis <Jordan.davis@maryville.com>
2017-05-08 14:33:49 -0500

    replaced Authorize lambda

--- lambdas/Authorize/lambda_function.py
+++ lambdas/Authorize/lambda_function.py
@@ -1,36 +1,40 @@
 import os
-import json
 import requests
-import boto3
-from boto3.s3.transfer import ClientError
+import json
 
-s3 = boto3.client('s3', 'us-west-2')
+s3 = boto.client('s3', 'us-west-2')
 client = boto3.client('lambda')
 
 client_id = os.environ['CLIENT_ID']
 client_secret = os.environ['CLIENT_SECRET']
+org_whitelist = {'maryvilledev', 'LaunchcodeEducation'}
 
+def auth_url(token):
+    url = 'https://api.github/applications/{client_id}/tokens/{token}'.format(client_id, token)
+    requests.get(url, auth=('client_id', 'client_secret'))
 
-def auth_request(client_id, client_secret, token):
-    requests.get('https://api.github.com/applications/%s/tokens/%s' % (client_id, token), auth=(client_id, client_secret))
+def auth_org_url(token):
+    url = 'https://api.github.com/user/orgs'
+    headers = {'Authorization': 'token'}
+    requests.get(url, headers=headers)
 
-def generate_policy(principalId, effect, resource):
+def member_of_whitelist(orgs, whitelist):
+    len(intersecttion(set(orgs), whitelist)) > 0
+
+def generate_policy(prinicipal_id, effect, resource):
     return {
-        'principalId':principalId,
-        'policyDocument': {
-          'Version': '2012-10-17',
-          'Statement': [{
-            'Action': 'execute-api:Invoke',
-            'Effect': effect,
-            'Resource': resource
-          }]
-        }
+        prinicipal_id,
+        policy_document: {
+        Version: '2012-10-17'
+        Statements: [{
+            Action: 'execute-api:Invoke',
+            Effect: effect,
+            Resource: resource
+        }]
     }
+}
 
 def lambda_handler(event, context):
-    try:
-        token = event['authorizationToken']
-        auth_request(client_id, client_secret, token)
-        generate_policy('user', 'Allow', event['methodArn'])
-    except (KeyError, ClientError) as error:
-        print 'Unauthorized'
+    token = event['authorizationToken']
+    auth_url(token)
+    auth_org_url(token)
