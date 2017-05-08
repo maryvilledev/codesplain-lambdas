ff2f7477e798c39b3adb5450a49669023c4b8249
Jordan Davis <Jordan.davis@maryville.com>
2017-05-08 15:55:26 -0500

    converted GithubAccessCodeGetter lambda to python

--- lambdas/GitHubAccessCodeGetter/lambda_function.py
+++ lambdas/GitHubAccessCodeGetter/lambda_function.py
@@ -2,39 +2,37 @@ import os
 import requests
 import json
 
-s3 = boto.client('s3', 'us-west-2')
-client = boto3.client('lambda')
-
 client_id = os.environ['CLIENT_ID']
 client_secret = os.environ['CLIENT_SECRET']
-org_whitelist = {'maryvilledev', 'LaunchcodeEducation'}
+org_whitelist = os.environ['ORG_WHITELIST'].split(';')
+ignore_whitelist = os.environ['IGNORE_WHITELIST']
 
-def auth_url(token):
-    url = 'https://api.github/applications/{client_id}/tokens/{token}'.format(client_id, token)
-    requests.get(url, auth=('client_id', 'client_secret'))
-
-def auth_org_url(token):
-    url = 'https://api.github.com/user/orgs'
-    headers = {'Authorization': 'token'}
-    requests.get(url, headers=headers)
-
-def member_of_whitelist(orgs, whitelist):
-    len(intersecttion(set(orgs), whitelist)) > 0
-
-def generate_policy(prinicipal_id, effect, resource):
+def generate_resp(code, body):
     return {
-        prinicipal_id,
-        policy_document: {
-        Version: '2012-10-17'
-        Statements: [{
-            Action: 'execute-api:Invoke',
-            Effect: effect,
-            Resource: resource
-        }]
+    'statusCode': code,
+    'headers': {'Access-Control-Allow-Origin': '*'},
+    'body': body
     }
-}
 
 def lambda_handler(event, context):
-    token = event['authorizationToken']
-    auth_url(token)
-    auth_org_url(token)
+    #Get the code from event body
+    code = json.loads(event)['body']['code']
+
+    #Get the token from GitHub
+    url = 'https://github.com/login/oauth.access_token'
+    data = {'client_id': client_id, 'client_secret': client_secret, 'code': code}
+    r = requests.get(url, data=data)
+    if r.status_code != 200:
+        return generate_resp(400, 'The authorization code is invalid')
+
+    #Get the user orgs
+    if ignore_whitelist.lower() != 'true':
+        url = 'https://api.github.com/user/orgs'
+        headers = {'Authorization': 'token'}
+        r = requests.get(url, headers=headers)
+        user_orgs = map(lambda x: x['login'], r.json())
+        if len(intersecttion(set(user_orgs), set(whitelist)) > 0:
+            return generate_resp(403, 'You are not a member of an organization authorized to use this application.')
+
+    #Return token and orgs to  user with code 200
+    return generate_resp(200, json.dumps({'token': token, 'orgs': user_orgs}))
