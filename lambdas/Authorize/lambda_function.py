596d7997f5f2034f218b40cd5931c046c83cd0a3
Jordan Davis <Jordan.davis@maryville.com>
2017-05-08 16:38:24 -0500

    fixed test errors & deleted python authorize test

--- lambdas/Authorize/lambda_function.py
+++ lambdas/Authorize/lambda_function.py
@@ -1,6 +1,6 @@
 import os
 import json
-from boto3.exceptions import ClientError
+from boto3.s3.transfer import ClientError
 import requests
 
 client_id = os.environ['CLIENT_ID']
@@ -8,13 +8,13 @@ client_secret = os.environ['CLIENT_SECRET']
 
 def generate_policy(prinicipal_id, effect, resource):
     return {
-        prinicipal_id,
-        policy_document: {
-        Version: '2012-10-17'
-        Statements: [{
-            Action: 'execute-api:Invoke',
-            Effect: effect,
-            Resource: resource
+        'prinicipal_id': prinicipal_id,
+        'policy_document': {
+            'Version': '2012-10-17'
+            'Statements': [{
+            'Action': 'execute-api:Invoke',
+            'Effect': effect,
+            'Resource': resource
         }]
     }
 }
