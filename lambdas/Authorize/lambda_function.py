ce0dfddaddb227f9f28eebf3cc2fb72273f70b9b
Dane Johnson <dane_johnson@ymail.com>
2017-05-10 12:35:40 -0500

    Fixed Jordan's spelling mistakes

--- lambdas/Authorize/lambda_function.py
+++ lambdas/Authorize/lambda_function.py
@@ -5,10 +5,10 @@ from requests.exceptions import HTTPError
 client_id = os.environ['CLIENT_ID']
 client_secret = os.environ['CLIENT_SECRET']
 
-def generate_policy(prinicipal_id, effect, resource):
+def generate_policy(principal_id, effect, resource):
     return {
-        'prinicipal_id': prinicipal_id,
-        'policy_document': {
+        'principalId': principal_id,
+        'policyDocument': {
             'Version': '2012-10-17',
             'Statements': [{
             'Action': 'execute-api:Invoke',
@@ -24,5 +24,5 @@ def lambda_handler(event, context):
     try:
         requests.get(url, auth=(client_id, client_secret)).raise_for_status()
     except HTTPError:
-        return {'body': 'Unauthorized'}
+        return {"errorMessage": "Unauthorized"} 
     return generate_policy('user', 'Allow', event['methodArn'])
