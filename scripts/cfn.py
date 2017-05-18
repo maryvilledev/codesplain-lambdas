a34012cbd60dddad406663ce30d752002101148f
Brian Hoehne <brian.hoehne@maryville.com>
2017-05-18 16:08:35 -0500

    fix code review notes

--- scripts/cfn.py
+++ scripts/cfn.py
@@ -4,14 +4,19 @@ import os
 import json
 import sys
 
-def main(arg1):
-    dateFormat = "[%H:%M %m.%d.%y]"
+DATE_FORMAT = "[%H:%M %m.%d.%y]"
+
+def printTime(timestamp):
+    return timestamp.strftime(DATE_FORMAT)
+
+
+def main(stack_name):
     rollback = False
 
 
     client = boto3.client('cloudformation', region_name='us-west-2')
     response = client.describe_stack_events(
-        StackName=arg1
+        StackName=stack_name
     )
     for event in response['StackEvents']:
         # The beginning of the current Stack Event
@@ -21,24 +26,24 @@ def main(arg1):
                 if rollback:
                     print 'Deleting Stack'
                     client.delete_stack(
-                        StackName=arg1
-                )
+                        StackName=stack_name
+                        )
                 return
         # Delet Stacks that were Rolled Back
         if event['ResourceStatus'] == 'ROLLBACK_COMPLETE':
             rollback = True
         # Catch and log stack failures
         if event['ResourceStatus'] == 'CREATE_FAILED' or event['ResourceStatus']  == 'UPDATE_FAILED':
-            print 'Stack Event failed ' + event['Timestamp'].strftime(dateFormat)
+            print 'Stack Event failed ' + printTime(vent['Timestamp'])
             print event['ResourceType'] + ': ' + event['ResourceStatusReason']
         # If Cloudformation Stack Complete or Update Complete, log and return
         if event['ResourceType'] == 'AWS::CloudFormation::Stack':
             if event['ResourceStatus']  == 'UPDATE_COMPLETE':
-                print event['LogicalResourceId'] + " updated " + event['Timestamp'].strftime(dateFormat)
+                print event['LogicalResourceId'] + " updated " + printTime(event['Timestamp'])
                 return
 
             elif event['ResourceStatus'] == 'CREATE_COMPLETE':
-                print event['LogicalResourceId'] + " created" + event['Timestamp'].strftime(dateFormat)
+                print event['LogicalResourceId'] + " created" + printTime(event['Timestamp'])
                 return
 
 
