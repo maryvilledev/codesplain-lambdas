353bd1cc80a15134f119cdcb7d6fe0588f786a1e
Hopding <andrew.dillon.j@gmail.com>
2017-05-25 11:24:09 -0500

    Make DumpSnippets return URL to zip file in S3

--- lambdas/DumpSnippets/lambda_function.py
+++ lambdas/DumpSnippets/lambda_function.py
@@ -6,33 +6,48 @@ import base64
 
 # Initialization
 s3 = boto3.client('s3', 'us-west-2')
-bucket_name = os.environ['BucketName']
-if bucket_name == None:
+try:
+    bn = os.environ['BucketName']
+except:
     print 'Must specify "BucketName" env var!'
     raise error
-s3_bucket = boto3.resource('s3').Bucket(bucket_name)
+try:
+    zb = os.environ['ZipsBucket']
+except KeyError:
+    zb = 'codesplain-zips'
+snippets_bucket = boto3.resource('s3').Bucket(bn)
+zips_bucket     = boto3.resource('s3').Bucket(zb)
 
-def b64_zip_snippets(snippet_keys):
-    """Writes the contents of the snippets with given keys to a zip file
-       and returns the raw base64 encoded zip file"""
+
+def zip_snippets(snippet_keys):
+    """Writes the contents of the snippets with given keys to a zip file,
+       returns the path to the zip."""
     zip_path = '/tmp/dump.zip'
     with zipfile.ZipFile(zip_path, 'w') as dump_zip:
         for key in snippet_keys:
             snippet  = snippet_contents(key)
             filename = os.path.basename(key)
             dump_zip.writestr(filename, snippet)
-    with open(zip_path, 'r') as dump_zip:
-        return base64.b64encode(dump_zip.read())
+    return zip_path
+
+def move_to_zips_bucket(source, dest):
+    """Copies the source file to the zips_bucket under dest path, returns
+       publicly-accessible URL for that new resource."""
+    zips_bucket.upload_file(source, dest)
+    return s3.generate_presigned_url('get_object', Params = {
+        'Bucket': zips_bucket.name,
+        'Key': dest,
+    })
 
 def snippet_contents(key):
     """Returns contentnts of specified snippet as string"""
-    return s3.get_object(Bucket=bucket_name, Key=key)['Body'].read()
+    return s3.get_object(Bucket=snippets_bucket.name, Key=key)['Body'].read()
 
 def user_snippet_keys(user):
     """Returns the object keys ("absolute paths") to each of
        a user's snippets, including their index.json file"""
     keys = []
-    for obj in s3_bucket.objects.all():
+    for obj in snippets_bucket.objects.all():
         if user == owner(obj.key):
             keys.append(obj.key)
     return keys
@@ -45,9 +60,11 @@ def owner(snippet_path):
 
 def lambda_handler(event, context):
     """Called by AWS"""
-    user    = event['pathParameters']['user_id']
-    keys    = user_snippet_keys(user)
-    b64_zip = b64_zip_snippets(keys)
+    user     = event['pathParameters']['user_id']
+    dest     = user + '/snippets.zip'
+    keys     = user_snippet_keys(user)
+    zip_path = zip_snippets(keys)
+    url      = move_to_zips_bucket(zip_path, dest)
 
     headers = {
         'Access-Control-Allow-Origin': '*',
@@ -56,5 +73,5 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'headers':    headers,
-        'body':       b64_zip,
+        'body':       url,
     }
